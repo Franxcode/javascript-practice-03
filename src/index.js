a7e8d77ade7e714d36ae259c547ddd63c9c7770f
Francisco Oliva <francisco.j.oliva@gmail.com>
2022-02-10 22:53:20 -0300

    APIs, http requests, cloudinary, image post.

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { init } from './js/archivos-page';
+init();
+// import { init } from "./js/chistes-page";
+// import { init } from "./js/usuarios-page";
+// init();
+
+// import * as CRUD from './js/crud-provider';
+
+// CRUD.getUsuario(2).then(console.log);
+
+// CRUD.createUsuario({
+//     name: 'Francisco',
+//     job: 'Carpintero'
+// }).then( console.log );
+
+// CRUD.updateUsuario(1, {
+//     name: 'Rebecca',
+//     job: 'Developer'
+// }).then( console.log );
+
+// CRUD.deleteUsuario(1).then( console.log );
---
+++ src/js/http-provider.js
@@ -0,0 +1,61 @@
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+const userUrl = 'https://reqres.in/api/users?page=2';
+
+// Cloudinary
+const cloudPreset = 'ml_default';
+const cloudUrl = 'https://api.cloudinary.com/v1_1/franx/image/upload';
+
+const obtenerChiste = async() => {
+
+    try {
+        const resp = await fetch(jokeUrl);
+        if ( !resp.ok ) throw 'No se pudo realizar la peticion';
+
+        const { icon_url, id, value } = await resp.json();
+        return { icon_url, id, value };
+         
+    } catch (error) {
+        throw error;
+    }
+};
+
+const obtenerUsuarios = async() => {
+
+    try {
+        const resp = await fetch(userUrl);
+        const { data:usuarios } = await resp.json();
+        return usuarios;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// ArchivoSubir :: File
+const subirImagen = async( archivoSubir ) => {
+    const formData = new FormData();
+    formData.append('upload_preset', cloudPreset);
+    formData.append('file', archivoSubir);
+
+    try {
+        const resp = await fetch(cloudUrl, {
+            method: 'POST',
+            body: formData
+        });
+
+        if(resp.ok){
+            const cloudResp = await resp.json();
+            console.log(cloudResp);
+            return cloudResp.secure_url;
+        }else{
+            throw await resp.json();
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+export {
+    obtenerChiste,
+    obtenerUsuarios,
+    subirImagen
+}
